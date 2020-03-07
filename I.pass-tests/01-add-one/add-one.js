d8fe068216341126c8f6c80a2b049b4b675c6a60
ayseakyol <ayseakyol.22309@gmail.com>
2020-03-07 16:07:13 +0100

    1

--- I.pass-tests/01-add-one/add-one.js
+++ I.pass-tests/01-add-one/add-one.js
@@ -1,1 +1,7 @@
-module.exports = function(numbers) {};
+module.exports = function(numbers) {
+  let newArray = [];
+  for (var i = 0; i < numbers.length; i++) {
+    newArray.push(numbers[i] + 1);
+  }
+  return newArray;
+};
